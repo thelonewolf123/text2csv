4d04e6b7dd240a0a081feef6fc04725561b90d6e
thelonewolf123 <40377888+thelonewolf123@users.noreply.github.com>
2020-02-06 20:58:14 +0530

    add command line args

--- text2csv.py
+++ text2csv.py
@@ -2,12 +2,20 @@
 
 import pandas as pd
 import re
+import os
 
-file = open('sample.txt')
-text = file.read()
 
-email = re.findall(r'([A-Za-z0-9]*@[a-z]*.\w*)',text)
+if(len(os.argv) == 2)
+	
+	file = open(os.argv[1])
+	text = file.read()
 
-print(email)
-email_list = pd.DataFrame(email)
-email_list.to_csv('emails.csv')
+	email = re.findall(r'([A-Za-z0-9]*@[a-z]*.\w*)',text)
+
+	print(email)
+	email_list = pd.DataFrame(email)
+	email_list.to_csv('emails.csv')
+
+else
+	
+	print(f"{os.argv[0] filename.txt}")
